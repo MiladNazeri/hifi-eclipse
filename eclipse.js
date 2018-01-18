08f91b491740356741aac459de65fd581827f52d
milad <milad@highfidelity.io>
2018-01-17 18:53:17 -0800

    added moon

--- eclipse.js
+++ eclipse.js
@@ -1,6 +1,20 @@
 (function(){
+    // 500,0,500
+    // {"x":2955.887451171875,"y":22.427629470825195,"z":2202.44775390625}
+    var startEclipsePosition,
+        endEclipsePosition,
+        entityIDs,
+        myEntities,
+        target;
 
-    var startEclipsePosition;
+    function log(describer, obj) {
+        obj = obj || '';
+        print('&======');
+        print(APP_NAME + ": \n" + describer);
+        print(JSON.stringify(obj));
+        print('======&');
+    }
+
 
     function onUpdate() {
 
@@ -8,3 +22,38 @@
 
     Script.update.connect(onUpdate);
 });
+
+function getMyEntities(myEntities){
+    entityIDs = Entities.findEntities(MyAvatar.position, 1000);
+    for (var i = 0; i < entityIDs.length; i++){
+        props = Entities.getEntityProperties(entityIDs[i]);
+        if (props.name.indexOf(target) !== -1 ||
+            props.name.indexOf(uuidTarget) !== -1) {
+                myEntities.push({name:props.name, entityID:entityIDs[i]});
+        };
+    };
+}
+
+function listMyEntities(myEntities){
+    print("Listing: " + myEntities.length);
+    for (var i = 0; i < myEntities.length; i++) {
+        print(myEntities[i].name + " " + myEntities[i].entityID);
+    };
+}
+
+function deleteMyEntities(myEntities){
+    printDebug("Deleting: " + myEntities.length);
+    for (var i = 0; i < myEntities.length; i++) {
+        printDebug(myEntities[i].name + " " + myEntities[i].entityID);
+        Entities.deleteEntity(myEntities[i].entityID);
+    };
+}
+
+function MyIdentifier(myEntity){
+    return myEntity = myEntity+" "+uuidTarget;
+}
+
+function newUuidTarget(){
+    allUuidTargets.push(uuidTarget);
+    uuidTarget = Uuid.generate();
+}
